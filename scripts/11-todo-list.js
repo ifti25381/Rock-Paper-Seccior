ba9ff03e8e52579c13ea6320745d5d49032eb974
ifti25381 <87604406+ifti25381@users.noreply.github.com>
2025-11-21 04:19:54 +0500

    ok

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,5 +1,18 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash dishes']; //Initial todo list
 
+renderTodoList(); // Initial rendering of the todo list
+
+function renderTodoList() {
+
+  let todoListHTML = ''; // Variable to hold the HTML representation
+
+  for (let i = 0; i < todoList.length; i++) {
+    const html = `<p>${todoList[i]}</p>`;
+    todoListHTML += html;
+  }
+  console.log(todoListHTML); // Log the HTML representation of the todo list
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input'); // Select the input element
 
@@ -8,5 +21,6 @@ function addTodo() {
   todoList.push(name); // Add the new todo item to the array
   console.log(todoList);
   inputElement.value = ''; // To clear the input field after adding
+  renderTodoList(); // Re-render the todo list to include the new item
 
 }
