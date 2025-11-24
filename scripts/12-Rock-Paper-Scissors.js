cbf4df0a13c449bb54966204ac54687a5b29a968
ifti25381 <87604406+ifti25381@users.noreply.github.com>
2025-11-24 05:34:28 +0500

    lesson 12

---
+++ scripts/12-Rock-Paper-Scissors.js
@@ -0,0 +1,59 @@
+ const scores = {
+            wins: 0,
+            losses: 0,
+            ties: 0
+        };
+
+        updateScores();
+
+        // making autoplay function by using setInterval method
+        function autoPlay() {
+            setInterval(function (){
+                const playGame = pickComputerMove();
+                playerMove(playGame, pickComputerMove());
+            }, 1500);
+        }
+        function pickComputerMove() {
+            let randomNumber = Math.random();
+            if (randomNumber < 0.34) {
+                return 'rock';
+            } else if(randomNumber <= 0.67) {
+                return 'paper';
+            } else {
+                return 'scissors';
+            } return randomNumber;
+        }
+
+        function playerMove(playerMove, computerMove) {
+
+        if (playerMove === computerMove){
+            result = "Tie";
+        } else if (playerMove === 'rock' && computerMove === 'scissors' ||
+                playerMove === 'paper' && computerMove === 'rock' ||
+                playerMove === 'scissors' && computerMove === 'paper')
+        { result = 'You win!';}
+
+        else{
+            result = 'You lose';
+        }
+        if (result === 'You win!'){
+            scores.wins += 1;
+        } else if (result === 'You lose'){
+            scores.losses += 1;
+        } else {
+            scores.ties += 1;
+        }
+        console.log(scores);
+
+        updateScores();
+        
+        document.querySelector('.js-moves').innerHTML = `You Chose <img class="move-icon" src="images/${playerMove}-emoji.png" alt="This is rock emoji picture"> 
+        computer chose <img class="move-icon" src="images/${computerMove}-emoji.png" alt="This is scissors emoji picture">`;
+        
+        document.querySelector('.js-result').innerHTML = result;                   
+
+        }
+        function updateScores(){
+            document.querySelector('.js-scores') .innerHTML = `Wins: ${scores.wins}, Losses: ${scores.losses}, Ties: ${scores.ties}`;
+        }
+            
