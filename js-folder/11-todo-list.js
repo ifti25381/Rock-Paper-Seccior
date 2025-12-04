d46fd5d708bbb3d47aa308ac437d8b876a0dd297
ifti25381 <87604406+ifti25381@users.noreply.github.com>
2025-12-04 18:31:20 +0500

    refreshing

--- js-folder/11-todo-list.js
+++ js-folder/11-todo-list.js
@@ -1,4 +1,18 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash dishes'];
+
+renderTodoList();
+
+
+function renderTodoList () {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML = html + todoListHTML;
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+  }
+}
 
 function addTodo () {
   const inputElement = document.querySelector('.js-name-input');
@@ -6,4 +20,5 @@ function addTodo () {
   todoList.push(name);
   console.log(todoList);
   inputElement.value = '';
+  renderTodoList();
 }
