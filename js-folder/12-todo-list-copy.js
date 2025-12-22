2112f41a03d89cc108a5aaa78334e3469b0f734c
ifti25381 <87604406+ifti25381@users.noreply.github.com>
2025-12-22 19:51:50 +0500

    12-Rock-Papaer-Scissors-with-CSS-for-Eventlistner
    removing onclick attribute from HTML and using eventListner into java file

---
+++ js-folder/12-todo-list-copy.js
@@ -0,0 +1,82 @@
+let todoListSimple = [];
+
+function addTodoSimple() {
+  const inputElement = document.querySelector('.js-todo-name-simple');
+  const name = inputElement.value
+
+  todoListSimple.push(name);
+
+  inputElement.value = '';
+  console.log(todoListSimple);
+}
+
+let todoListSimple2 = [];
+
+function addTodoSimple2() {
+  const inputElement = document.querySelector('.js-todo-name-simple-2');
+  const name = inputElement.value
+
+  todoListSimple2.push(name);
+  inputElement.value = '';
+
+  renderTodoSimple2();
+}
+
+function renderTodoSimple2() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoListSimple2.length; i++) {
+    const todo = todoListSimple2[i];
+    todoListHTML += `
+      <p>${todo}</p>
+    `;
+  }
+
+  document.querySelector('.js-todo-list-simple-2')
+    .innerHTML = todoListHTML;
+}
+
+let todoList = [];
+renderTodoList();
+
+function addTodo() {
+  const todoNameElem = document.querySelector('.js-todo-name');
+  const name = todoNameElem.value;
+
+  const todoDueDateElem = document.querySelector('.js-todo-due-date');
+  const dueDate = todoDueDateElem.value;
+
+  todoList.push({
+    name,
+    dueDate
+  });
+
+  renderTodoList();
+  todoNameElem.value = '';
+  todoDueDateElem.value = '';
+}
+
+function deleteTodo(index) {
+  todoList.splice(index, 1);
+  renderTodoList();
+}
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  todoList.forEach((todoObject, index) => {
+    const { name, dueDate } = todoObject;
+
+    todoListHTML += `
+      <div class="todo-row">
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button class="delete-todo-button" onclick="deleteTodo(${index})">
+          Delete
+        </button>
+      </div>
+    `;
+  });
+
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
